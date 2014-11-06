ca5ce1ee1c49fa3540b3301237432936cdbb960c
ramittal <ramittal@visa.com>
2014-11-06 02:15:18 -0800

    Adding cygwin installation checks for scripts

--- scripts/generateControllersFromTemplate.cmd
+++ scripts/generateControllersFromTemplate.cmd
@@ -1,5 +1,13 @@
 @ECHO OFF
 
+CALL "%~dp0\validateCygwinBinaries.cmd"
+IF "1"=="%ERRORLEVEL%" (
+    @ECHO Invalid or incomplete Cygwin installation. Install cygwin and its components viz.
+    @ECHO grep sed perl cut touch wget sort
+    EXIT /b 1
+)
+SET CYGWIN_EXE=%CYGWIN_HOME%\bin
+
 @ECHO Starting %DATE%-%TIME%
 
 SET CYGWIN=NODOSFILEWARNING
@@ -18,27 +26,27 @@ IF EXIST "%CD%\log" (
 )
 
 IF NOT EXIST "%SRCDIR%" (
-	@ECHO Unable to find "%SRCDIR%"
-	EXIT /b 1
+    @ECHO Unable to find "%SRCDIR%"
+    EXIT /b 1
 )
 @ECHO Identifying Requests/Responses to process from "%SRCDIR%"
-DIR /s %SRCDIR%\%GENFOLDER%\*.java | grep -i -e "request\.java" -e "response\.java" > %SRCLOG%0.log
+DIR /s %SRCDIR%\%GENFOLDER%\*.java | "%CYGWIN_EXE%\grep.exe" -i -e "request\.java" -e "response\.java" > %SRCLOG%0.log
 DIR /s %SRCDIR%\%CONTROLLERFOLDER%\*Controller.java > %CNTLOG%0.log
 
 @ECHO Cleaning up paths in Sources and Controllers
-cut -c40- %SRCLOG%0.log  | sort -u | grep -i "\.java" | cut -d. -f1 | sort -u > %SRCLOG%1.log
-cut -c40- %CNTLOG%0.log  | sort -u | grep -i "\.java" | cut -d. -f1 | sort -u > %CNTLOG%.log
+"%CYGWIN_EXE%\cut.exe" -c40- %SRCLOG%0.log  | "%CYGWIN_EXE%\sort.exe" -u | "%CYGWIN_EXE%\grep.exe" -i "\.java" | "%CYGWIN_EXE%\cut.exe" -d. -f1 | "%CYGWIN_EXE%\sort.exe" -u > %SRCLOG%1.log
+"%CYGWIN_EXE%\cut.exe" -c40- %CNTLOG%0.log  | "%CYGWIN_EXE%\sort.exe" -u | "%CYGWIN_EXE%\grep.exe" -i "\.java" | "%CYGWIN_EXE%\cut.exe" -d. -f1 | "%CYGWIN_EXE%\sort.exe" -u > %CNTLOG%.log
 
 @ECHO Getting Unique Request/Responses
-grep -i -e "request *$" -e "response *$" %SRCLOG%1.log > %SRCLOG%2.log
+"%CYGWIN_EXE%\grep.exe" -i -e "request *$" -e "response *$" %SRCLOG%1.log > %SRCLOG%2.log
 
 @ECHO Identifying Object names
-perl -pi -w -e 's/Request *$//g;'  %SRCLOG%2.log
-perl -pi -w -e 's/Response *$//g;' %SRCLOG%2.log
-sort -u %SRCLOG%2.log      > %SRCLOG%3.log
+"%CYGWIN_EXE%\perl.exe" -pi -w -e 's/Request *$//g;'  %SRCLOG%2.log
+"%CYGWIN_EXE%\perl.exe" -pi -w -e 's/Response *$//g;' %SRCLOG%2.log
+"%CYGWIN_EXE%\sort.exe" -u %SRCLOG%2.log      > %SRCLOG%3.log
 
 @ECHO Fixing Controllers
-perl -pi -w -e 's/Controller *$//g;' %CNTLOG%.log
+"%CYGWIN_EXE%\perl.exe" -pi -w -e 's/Controller *$//g;' %CNTLOG%.log
 
 @REM Create backup for later comparison
 COPY %SRCLOG%3.log %SRCLOG%4.log >NUL
@@ -47,40 +55,40 @@ COPY %CNTLOG%.log  %CNTLOG%9.log >NUL
 @ECHO Removing ExistingControllers From Request/Response List
 @ECHO From File
 FOR /F %%X IN (%CNTLOG%.log) DO (
-	@ECHO Processing "%%X"
-	perl -pi -w -e 's/^\b%%X\b *$//g;' %SRCLOG%3.log  	
+    @ECHO Processing "%%X"
+    "%CYGWIN_EXE%\perl.exe" -pi -w -e 's/^\b%%X\b *$//g;' %SRCLOG%3.log      
 )
 
 @ECHO From BlackList
 FOR %%X IN (ANetApi Error Ids XXDoNotUseDummy) DO (
-	@ECHO Processing BlackList "%%X"
-	perl -pi -w -e 's/^\b%%X\b *$//g;' %SRCLOG%3.log
+    @ECHO Processing BlackList "%%X"
+    "%CYGWIN_EXE%\perl.exe" -pi -w -e 's/^\b%%X\b *$//g;' %SRCLOG%3.log
 )
 
 @ECHO Creating Final List of Request/Response to generate code
-sort -u %SRCLOG%3.log   > %SRCLOG%.log
+"%CYGWIN_EXE%\sort.exe" -u %SRCLOG%3.log   > %SRCLOG%.log
 
 FOR /F %%x IN (%SRCLOG%.log ) DO (
-	IF EXIST "%SRCDIR%\%CONTROLLERFOLDER%\%%xController.java" (
-		@ECHO "%SRCDIR%\%CONTROLLERFOLDER%\%%xController.java" exists, Creating New
-		COPY resources\ControllerTemplate.javat    "%SRCDIR%\%CONTROLLERFOLDER%\%%xController.new"
-		perl -pi -w -e 's/APICONTROLLERNAME/%%x/g;' %SRCDIR%\%CONTROLLERFOLDER%\%%xController.new
-	) ELSE (
-		@ECHO Generating Code for "%SRCDIR%\%CONTROLLERFOLDER%\%%xController.java"
-		COPY resources\ControllerTemplate.javat    "%SRCDIR%\%CONTROLLERFOLDER%\%%xController.java"
-		perl -pi -w -e 's/APICONTROLLERNAME/%%x/g;' %SRCDIR%\%CONTROLLERFOLDER%\%%xController.java
-	)
+    IF EXIST "%SRCDIR%\%CONTROLLERFOLDER%\%%xController.java" (
+        @ECHO "%SRCDIR%\%CONTROLLERFOLDER%\%%xController.java" exists, Creating New
+        COPY resources\ControllerTemplate.javat    "%SRCDIR%\%CONTROLLERFOLDER%\%%xController.new"
+        "%CYGWIN_EXE%\perl.exe" -pi -w -e 's/APICONTROLLERNAME/%%x/g;' %SRCDIR%\%CONTROLLERFOLDER%\%%xController.new
+    ) ELSE (
+        @ECHO Generating Code for "%SRCDIR%\%CONTROLLERFOLDER%\%%xController.java"
+        COPY resources\ControllerTemplate.javat    "%SRCDIR%\%CONTROLLERFOLDER%\%%xController.java"
+        "%CYGWIN_EXE%\perl.exe" -pi -w -e 's/APICONTROLLERNAME/%%x/g;' %SRCDIR%\%CONTROLLERFOLDER%\%%xController.java
+    )
 )
 @REM Identify Obsolete Controllers
 @ECHO From Request/ResponseList
 FOR /F %%X IN (%SRCLOG%4.log) DO (
-	@ECHO Processing "%%X"
-	perl -pi -w -e 's/%%X *$//g;' %CNTLOG%9.log  	
+    @ECHO Processing "%%X"
+    "%CYGWIN_EXE%\perl.exe" -pi -w -e 's/%%X *$//g;' %CNTLOG%9.log      
 )
 @ECHO Following are Obsolete Controllers
-sort -u %CNTLOG%9.log
+"%CYGWIN_EXE%\sort.exe" -u %CNTLOG%9.log
 DEL /s *.bak 1>NUL 2>&1
 
 ENDLOCAL
- 	
+     
 @ECHO FINISHED %DATE%-%TIME%
--- scripts/generateObjectsFromXsd.cmd
+++ scripts/generateObjectsFromXsd.cmd
@@ -1,4 +1,20 @@
 @ECHO OFF
+
+CALL "%~dp0\validateCygwinBinaries.cmd"
+IF "1"=="%ERRORLEVEL%" (
+    @ECHO Invalid or incomplete Cygwin installation. Install cygwin and its components viz.
+    @ECHO grep sed perl cut touch wget sort
+    EXIT /b 1
+)
+SET CYGWIN_EXE=%CYGWIN_HOME%\bin
+
+where xjc.exe > NUL 2>&1
+IF "1"=="%ERRORLEVEL%" (
+    @ECHO Unable to find xjc.exe in the path. Locate it and add it directory to the path
+    @ECHO Usually it is found under JDK bin viz. "C:\custom\Java180_05\bin\xjc.exe"
+    EXIT /b 1
+)
+
 SETLOCAL
 @ECHO Starting %DATE%-%TIME%
 
@@ -8,18 +24,18 @@ SET LOCALWSDL=%TEMP%\AnetApiSchema.wsdl
 SET selection=N
 CHOICE /C YN  /T 10 /D N /M "Fetch and update Schema/WSDL file from remote server?"
 IF "%ERRORLEVEL%"=="1" (
-	@ECHO Fetching Schema/WSDL files 
-	SET %ERRORLEVEL%=
-	CALL "%~dp0\getXsdWsdl.cmd" %LOCALXSD% %LOCALWSDL%
-	SET ERRORCODE=%ERRORLEVEL%
-	@ECHO GetXsdWsdl Call Exit Code:%ERRORCODE%
-	IF NOT "%ERRORLEVEL%"=="0" (
-	   @ECHO Error fetching source files
-	   @ECHO ##### ***** $$$$$ CHECK FOR ERROR $$$$$ ***** #####
-	   REM EXIT /b 1
+    @ECHO Fetching Schema/WSDL files 
+    SET %ERRORLEVEL%=
+    CALL "%~dp0\getXsdWsdl.cmd" %LOCALXSD% %LOCALWSDL%
+    SET ERRORCODE=%ERRORLEVEL%
+    @ECHO GetXsdWsdl Call Exit Code:%ERRORCODE%
+    IF NOT "%ERRORLEVEL%"=="0" (
+       @ECHO Error fetching source files
+       @ECHO ##### ***** $$$$$ CHECK FOR ERROR $$$$$ ***** #####
+       REM EXIT /b 1
     )
 ) ELSE (
-	@ECHO Schema/WSDL files have not been updated!
+    @ECHO Schema/WSDL files have not been updated!
 )
 SET XSDSRCDIR=src/main/java/
 SET XSDPACKAGE=net.authorize.api.contract.v1
@@ -29,20 +45,20 @@ SET WSDLSRCDIR=src/wsdlgen/java/
 SET WSDLPACKAGE=net.authorize.api.contract.v1
 
 IF NOT EXIST "%LOCALXSD%" (
-	@ECHO Unable to find "%LOCALXSD%"
-	EXIT /b 1
+    @ECHO Unable to find "%LOCALXSD%"
+    EXIT /b 1
 )
 IF NOT EXIST "%LOCALWSDL%" (
-	@ECHO Unable to find "%LOCALWSDL%"
-	@REM EXIT /b 1
+    @ECHO Unable to find "%LOCALWSDL%"
+    @REM EXIT /b 1
 )
 @ECHO Validating target folder "%XSDSRCDIR%"
 IF NOT EXIST %XSDSRCDIR% (
-		MD "%XSDSRCDIR%"
+        MD "%XSDSRCDIR%"
 )
 @ECHO Validating target folder "%WSDLSRCDIR%"
 IF NOT EXIST %WSDLSRCDIR% (
-		MD "%WSDLSRCDIR%"
+        MD "%WSDLSRCDIR%"
 )
 
 @ECHO Generating sources from Schema: %XSD% in folder "%XSDSRCDIR%"
@@ -54,15 +70,15 @@ REM wsimport -keep -verbose -d "%WSDLSRCDIR%" -p "%WSDLPACKAGE%" -Xnocompile "%L
 
 @ECHO Adding Serializable to the Base Request/Response
 FOR %%x IN (Request Response) DO (
-	@ECHO Processing "%%x" in file %XSDSRCDIR%%XSDGENFOLDER%\ANetApi%%x.java
-	IF EXIST "%XSDSRCDIR%%XSDGENFOLDER%\ANetApi%%x.java" (
-		REM DIR /s/b "%XSDSRCDIR%%XSDGENFOLDER%\ANetApi%%x.java" 
-		REM FINDSTR /i /s /c:"public class" "ANetApi%%x.java" 
-		perl -pi -w -e 's/public class ANetApi%%x */public class ANetApi%%x implements java.io.Serializable /g;'  %XSDSRCDIR%%XSDGENFOLDER%/ANetApi%%x.java
-		FINDSTR /i /s /c:"public class" "ANetApi%%x.java" 
-	) ELSE (
-		@ECHO File "%XSDSRCDIR%%XSDGENFOLDER%\ANetApi%%x.java"  Does not exist
-	)
+    @ECHO Processing "%%x" in file %XSDSRCDIR%%XSDGENFOLDER%\ANetApi%%x.java
+    IF EXIST "%XSDSRCDIR%%XSDGENFOLDER%\ANetApi%%x.java" (
+        REM DIR /s/b "%XSDSRCDIR%%XSDGENFOLDER%\ANetApi%%x.java" 
+        REM FINDSTR /i /s /c:"public class" "ANetApi%%x.java" 
+        "%CYGWIN_EXE%\perl.exe" -pi -w -e 's/public class ANetApi%%x */public class ANetApi%%x implements java.io.Serializable /g;'  %XSDSRCDIR%%XSDGENFOLDER%/ANetApi%%x.java
+        FINDSTR /i /s /c:"public class" "ANetApi%%x.java" 
+    ) ELSE (
+        @ECHO File "%XSDSRCDIR%%XSDGENFOLDER%\ANetApi%%x.java"  Does not exist
+    )
 )
 DEL /q /s *.bak
 ENDLOCAL
