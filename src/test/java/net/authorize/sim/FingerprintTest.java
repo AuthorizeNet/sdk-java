b6f831a0f7b7554085681ded862e2c4dc577c32c
brianmc <bmcmanus@authorize.net>
2015-05-05 22:04:27 -0700

    Added currency support to SIM fingerprint

--- src/test/java/net/authorize/sim/FingerprintTest.java
+++ src/test/java/net/authorize/sim/FingerprintTest.java
@@ -25,6 +25,18 @@ public class FingerprintTest extends UnitTestData {
 	}
 
 	@Test
+	public void createFingerprintWithCurrency() {
+		Fingerprint fingerprint = Fingerprint.createFingerprint("loginID",
+				"transactionKey", 1, "1.99", "EUR");
+
+		Assert.assertNotNull(fingerprint);
+		Assert.assertNotNull(fingerprint.getFingerprintHash());
+		Assert.assertEquals(32, fingerprint.getFingerprintHash().length());
+		Assert.assertTrue(fingerprint.getSequence() >= 0);
+		Assert.assertTrue(fingerprint.getTimeStamp() > 0);
+	}
+
+	@Test
 	public void createFingerprintWithObjectFields() {
 		Fingerprint fingerprint = Fingerprint.createFingerprint(merchant, 1,
 				new BigDecimal(1.99));
