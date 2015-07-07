1791930a8388e4b25ea69f3b45ed6284e150fb48
Calvin Brown <calvin@kairuconsulting.com>
2015-07-07 10:40:52 -0400

    PayPalExpress

---
+++ src/sample code/PayPalExpress/PayPalAuthorizationCapture.java
@@ -0,0 +1,10 @@
+package PayPalExpress;
+
+public class PayPalAuthorizationCapture {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/sample code/PayPalExpress/PayPalCredit.java
@@ -0,0 +1,10 @@
+package PayPalExpress;
+
+public class PayPalCredit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
