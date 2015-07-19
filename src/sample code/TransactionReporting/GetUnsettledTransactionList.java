43f840f9fa75e1961a8ba1b4d64ee9493339383d
Calvin Brown <calvin@kairuconsulting.com>
2015-07-19 15:41:48 -0400

    TransactionReporting classes

---
+++ src/sample code/TransactionReporting/GetUnsettledTransactionList.java
@@ -0,0 +1,10 @@
+package TransactionReporting;
+
+public class GetUnsettledTransactionList {
+
+	public static void main(String apiLoginId, String transactionKey) {
+		// TODO Auto-generated method stub
+		System.out.println("Get Batch Statistics Sample");
+	}
+
+}
