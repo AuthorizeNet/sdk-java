febc247ec96587e460196c226784bf8c817a542b
mkienenb <mkienenb@gmail.com>
2015-11-13 16:41:21 -0500

    hostedProfileSettings is an optional parameter for getHostedProfilePageRequest but SDK requires it

--- src/main/java/net/authorize/api/controller/GetHostedProfilePageController.java
+++ src/main/java/net/authorize/api/controller/GetHostedProfilePageController.java
@@ -17,7 +17,7 @@ public class GetHostedProfilePageController extends ApiOperationBase<GetHostedPr
 		
 		//validate required fields		
 		if ( null == request.getCustomerProfileId()) throw new NullPointerException("CustomerProfileIdcannot be null");
-		if ( null == request.getHostedProfileSettings()) throw new NullPointerException("HostedProfileSettings cannot be null");
+		// HostedProfileSettings is not a required field
 		
 		//validate not-required fields		
 		//creditCardOne.setCardCode("");
