1b79ed37bc97caf3759e5052d405023200b9427a
Zalak Shah <zalshah@visa.com>
2015-11-13 12:04:10 -0800

    CreateCustomerProfileController disallows creating profile without payment methods (#64)

--- src/main/java/net/authorize/api/controller/CreateCustomerProfileController.java
+++ src/main/java/net/authorize/api/controller/CreateCustomerProfileController.java
@@ -19,30 +19,6 @@ public class CreateCustomerProfileController extends ApiOperationBase<CreateCust
 		//validate required fields		
 		if ( null == request.getProfile()) throw new NullPointerException("Profile cannot be null");
 		if ( null == request.getRefId()) throw new NullPointerException("RefId cannot be null");
-		if ( null == request.getValidationMode() || ValidationModeEnum.NONE == request.getValidationMode()) throw new NullPointerException("ValidationMode cannot be null");
-		if ( null == request.getProfile().getPaymentProfiles() || 0 == request.getProfile().getPaymentProfiles().size()) throw new NullPointerException("Payment Profile cannot be null or empty");
-		
-		//validate not-required fields		
-	    
-		
-		//creditCardOne.setCardCode("");
-
-		//keyBlock.setValue(value);
-
-		//paymentOne.setBankAccount(bankAccountOne);
-		//paymentOne.setTrackData(trackDataOne);
-		//paymentOne.setEncryptedTrackData(encryptedTrackDataOne);
-		//paymentOne.setPayPal( payPalOne);
-		
-//		driversLicenseOne = new DriversLicenseType();
-//		driversLicenseOne.setNumber(getRandomString("DLNumber"));
-//		driversLicenseOne.setState(getRandomString("WA"));
-//		driversLicenseOne.setDateOfBirth(nowString);
-
-//	    customerPaymentProfileOne.setBillTo(customerAddressOne);
-//	    customerPaymentProfileOne.setDriversLicense(driversLicenseOne);
-//	    customerPaymentProfileOne.setTaxId(getRandomString("XX"));
-
 	}
 
 	@Override
