e924732d868ea50c3510fc6793c7d58289aaa1b5
vijayabraj <vijayabraj@gmail.com>
2018-07-26 16:10:15 +0530

    Updating deprecation comments for old model code

--- src/main/java/net/authorize/aim/Transaction.java
+++ src/main/java/net/authorize/aim/Transaction.java
@@ -43,9 +43,9 @@ import org.apache.commons.logging.LogFactory;
  *
  * @deprecated since version 1.9.8
  * @deprecated We have reorganized and simplified the Authorize.Net API to ease integration and to focus on merchants' needs.
- * @deprecated We have deprecated AIM, ARB, CIM, and Reporting as separate options, in favor of AuthorizeNet::API.
+ * @deprecated We have deprecated AIM, ARB, CIM, and Reporting as separate options, in favor of AuthorizeNet::API (package: net.authorize.api.*).
  * @deprecated We have also deprecated SIM as a separate option, in favor of Accept Hosted. See https://developer.authorize.net/api/reference/features/accept_hosted.html for details on Accept Hosted.
- * @deprecated For details on AIM, see https://github.com/AuthorizeNet/sample-code-php/tree/master/PaymentTransactions.
+ * @deprecated For details on AIM, see https://github.com/AuthorizeNet/sample-code-java/tree/master/src/main/java/net/authorize/sample/PaymentTransactions.
  * @deprecated For details on the deprecation and replacement of legacy Authorize.Net methods, visit https://developer.authorize.net/api/upgrade_guide/.
  * 
  */
--- src/main/java/net/authorize/util/DeepCopy.java
+++ src/main/java/net/authorize/util/DeepCopy.java
@@ -16,11 +16,11 @@ import java.io.ObjectOutputStream;
  * make more sense to have copy(...) re-throw the exception.
  * 
  * @deprecated since version 1.9.8
- * @deprecated We have reorganized and simplified the Authorize.Net API to ease integration and to focus on merchants' needs.
- * @deprecated We have deprecated AIM, ARB, CIM, and Reporting as separate options, in favor of AuthorizeNet::API.
- * @deprecated We have also deprecated SIM as a separate option, in favor of Accept Hosted. See https://developer.authorize.net/api/reference/features/accept_hosted.html for details on Accept Hosted.
- * @deprecated For details on AIM, see https://github.com/AuthorizeNet/sample-code-php/tree/master/PaymentTransactions.
- * @deprecated For details on the deprecation and replacement of legacy Authorize.Net methods, visit https://developer.authorize.net/api/upgrade_guide/.
+ * @deprecated We have reorganized and simplified the Authorize.Net API to ease integration and to focus on merchants' needs.
+ * @deprecated We have deprecated AIM, ARB, CIM, and Reporting as separate options, in favor of AuthorizeNet::API (package: net.authorize.api.*).
+ * @deprecated We have also deprecated SIM as a separate option, in favor of Accept Hosted. See https://developer.authorize.net/api/reference/features/accept_hosted.html for details on Accept Hosted.
+ * @deprecated For details on AIM, see https://github.com/AuthorizeNet/sample-code-java/tree/master/src/main/java/net/authorize/sample/PaymentTransactions.
+ * @deprecated For details on the deprecation and replacement of legacy Authorize.Net methods, visit https://developer.authorize.net/api/upgrade_guide/.
  *
  */
 @Deprecated
