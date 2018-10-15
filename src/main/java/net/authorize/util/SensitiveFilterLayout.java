7ca888dae1ce7a57d70118012734d4518ec03ada
vijayabraj <vijayabraj@gmail.com>
2018-10-15 15:18:13 +0530

    Revert "Updating SensitiveLogger"
    This reverts commit a5f94540451d4db9695a4323edc432fccef6486f.

--- src/main/java/net/authorize/util/SensitiveFilterLayout.java
+++ src/main/java/net/authorize/util/SensitiveFilterLayout.java
@@ -24,59 +24,49 @@ public class SensitiveFilterLayout extends PatternLayout{
 	private static Gson gson;
 	
 	public SensitiveFilterLayout() throws UnsupportedEncodingException, FileNotFoundException, IOException {
-		try {
-			GsonBuilder gsonBuilder = new GsonBuilder();
-			gsonBuilder.registerTypeAdapter(SensitiveDataConfigType.class, new SensitiveTagsDeserializer());
-			gson = gsonBuilder.create();
-				
-			InputStream in = getClass().getResourceAsStream("/AuthorizedNetSensitiveTagsConfig.json"); 
-			BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-				SensitiveDataConfigType configType = gson.fromJson(reader, SensitiveDataConfigType.class);
-				cardPatterns = new Pattern[configType.sensitiveStringRegexes.length];
-
-		      for(int i = 0; i < configType.sensitiveStringRegexes.length; i++) {  
-			    	  cardPatterns[i] = Pattern.compile(configType.sensitiveStringRegexes[i]);
-			      }
-			      
-				int noOfSensitiveTags = configType.sensitiveTags.length;
-				tagPatterns     = new Pattern[noOfSensitiveTags];
-			    tagReplacements = new String[noOfSensitiveTags];
-	     		      
-	             for(int j=0; j<noOfSensitiveTags; j++) {
-			    	  String tagName     = configType.sensitiveTags[j].tagName;
-			    	  String pattern     = configType.sensitiveTags[j].pattern;
-			    	  String replacement = configType.sensitiveTags[j].replacement;
-			    	  
-			    	  if(pattern != null && !pattern.isEmpty())
-			    		  tagPatterns[j] = Pattern.compile("<"+tagName+">"+pattern+"</"+tagName+">");
-			    	  else
-			    		  tagPatterns[j] = Pattern.compile("<"+tagName+">"+".+"+"</"+tagName+">");
-			    	  tagReplacements[j] = "<"+tagName+">"+replacement+"</"+tagName+">";
-			      }
-	             if(reader!=null)
-	            	 reader.close();			
-		}
-		catch(Exception e) {
+		GsonBuilder gsonBuilder = new GsonBuilder();
+		gsonBuilder.registerTypeAdapter(SensitiveDataConfigType.class, new SensitiveTagsDeserializer());
+		gson = gsonBuilder.create();
 			
-		}
+		InputStream in = getClass().getResourceAsStream("/AuthorizedNetSensitiveTagsConfig.json"); 
+		BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+			SensitiveDataConfigType configType = gson.fromJson(reader, SensitiveDataConfigType.class);
+			cardPatterns = new Pattern[configType.sensitiveStringRegexes.length];
+
+	      for(int i = 0; i < configType.sensitiveStringRegexes.length; i++) {  
+		    	  cardPatterns[i] = Pattern.compile(configType.sensitiveStringRegexes[i]);
+		      }
+		      
+			int noOfSensitiveTags = configType.sensitiveTags.length;
+			tagPatterns     = new Pattern[noOfSensitiveTags];
+		    tagReplacements = new String[noOfSensitiveTags];
+     		      
+             for(int j=0; j<noOfSensitiveTags; j++) {
+		    	  String tagName     = configType.sensitiveTags[j].tagName;
+		    	  String pattern     = configType.sensitiveTags[j].pattern;
+		    	  String replacement = configType.sensitiveTags[j].replacement;
+		    	  
+		    	  if(pattern != null && !pattern.isEmpty())
+		    		  tagPatterns[j] = Pattern.compile("<"+tagName+">"+pattern+"</"+tagName+">");
+		    	  else
+		    		  tagPatterns[j] = Pattern.compile("<"+tagName+">"+".+"+"</"+tagName+">");
+		    	  tagReplacements[j] = "<"+tagName+">"+replacement+"</"+tagName+">";
+		      }
+             if(reader!=null)
+            	 reader.close();
 	}
 
 	@Override
 	public String format(LoggingEvent event) {
-		try {
-			if(event.getMessage() instanceof String) {
-				String message = event.getRenderedMessage();
-				String maskXmlMessage = SensitiveFilterLayout.maskSensitiveXmlString(message);
-				String maskCardNumber = SensitiveFilterLayout.maskCreditCards(maskXmlMessage);
-				
-				Throwable throwable = event.getThrowableInformation() != null ? event.getThrowableInformation().getThrowable() : null;
-				LoggingEvent maskedEvent = new LoggingEvent(event.fqnOfCategoryClass, Logger.getLogger(event.getLoggerName()), event.timeStamp, event.getLevel(), maskCardNumber, throwable);
-				return super.format(maskedEvent);
-			}					
-		}
-		catch(Exception e) {
+		if(event.getMessage() instanceof String) {
+			String message = event.getRenderedMessage();
+			String maskXmlMessage = SensitiveFilterLayout.maskSensitiveXmlString(message);
+			String maskCardNumber = SensitiveFilterLayout.maskCreditCards(maskXmlMessage);
 			
-		}
+			Throwable throwable = event.getThrowableInformation() != null ? event.getThrowableInformation().getThrowable() : null;
+			LoggingEvent maskedEvent = new LoggingEvent(event.fqnOfCategoryClass, Logger.getLogger(event.getLoggerName()), event.timeStamp, event.getLevel(), maskCardNumber, throwable);			
+			return super.format(maskedEvent);
+		}		
 		return null;
 	}
 
