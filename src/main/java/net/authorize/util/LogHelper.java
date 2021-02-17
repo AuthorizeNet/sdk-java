00b9e5be80ac27d2fdacfa7feef9b37381b2945a
Q <queila52000@gmail.com>
2021-02-17 05:54:43 -0500

    Update LogHelper.java

--- src/main/java/net/authorize/util/LogHelper.java
+++ src/main/java/net/authorize/util/LogHelper.java
@@ -1,6 +1,6 @@
 package net.authorize.util;
 
-import org.apache.commons.logging.Log;
+import org.apache.logging.log4j.Logger;
 
 public final class LogHelper {
 
@@ -9,27 +9,27 @@ public final class LogHelper {
 	private LogHelper() {
 	}
 
-	public static void debug(Log logger, String format, Object... arguments) {
+	public static void debug(Logger logger, String format, Object... arguments) {
 		String logMessage = getMessage(logger, format, arguments);
 		if ( null != logMessage) { logger.debug(logMessage); }
 	}
 
-	public static void error(Log logger, String format, Object... arguments) {
+	public static void error(Logger logger, String format, Object... arguments) {
 		String logMessage = getMessage(logger, format, arguments);
 		if ( null != logMessage) { logger.error(logMessage); }
 	}
 	
-	public static void info(Log logger, String format, Object... arguments) {
+	public static void info(Logger logger, String format, Object... arguments) {
 		String logMessage = getMessage(logger, format, arguments);
 		if ( null != logMessage) { logger.info(logMessage); }
 	}
 
-	public static void warn(Log logger, String format, Object... arguments) {
+	public static void warn(Logger logger, String format, Object... arguments) {
 		String logMessage = getMessage(logger, format, arguments);
 		if ( null != logMessage) { logger.warn(logMessage); }
 	}
 
-	private static String getMessage(Log logger, String format, Object... arguments) {
+	private static String getMessage(Logger logger, String format, Object... arguments) {
 		String logMessage = null;
 		
 		if ( null != logger && null != format && 0 < format.trim().length()) {
